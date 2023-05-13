2a4af034fd15e06a28f7332fc7ae8b99ea838aa9
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-05-13 13:45:15 -0300

    function creation to validate unknown types in typescript

--- src/UI/repository/todo.ts
+++ src/UI/repository/todo.ts
@@ -1,3 +1,8 @@
+//Repository certifica que todos os dados estão vindo corretos
+//Como em nossa aplicação pode ter mais de um lugar onde pode chamar o fetch
+//quebramos ele em um repository para ficar acessível e organizado em outras
+//partes do código
+
 interface TodoControllerGetParams {
     page: number;
     limit: number;
@@ -15,7 +20,9 @@ function get({
 }: TodoControllerGetParams): Promise<TodoRepositoryGetOutput> {
     return fetch("/api/todos").then(async (resServerOfTodos) => {
         const todosString = await resServerOfTodos.text();
-        const todosFromServer = JSON.parse(todosString).todos;
+        const todosFromServer = parseTodosFromServer(
+            JSON.parse(todosString)
+        ).todos; // para não retornar valores de tipos desconhecidos, cria-se essa função para validar os tipos
 
         const allTodos = todosFromServer;
         const startIndex = (page - 1) * limit;
@@ -24,9 +31,9 @@ function get({
         const totalPages = Math.ceil(allTodos.length / limit);
 
         return {
-            todos: allTodos,
-            total: totalPages,
-            pages: paginatedTodos,
+            todos: paginatedTodos,
+            total: allTodos.length,
+            pages: totalPages,
         };
     });
 }
@@ -43,6 +50,37 @@ interface Todo {
     done: boolean;
 }
 
-//Como em nossa aplicação pode ter mais de um lugar onde pode chamar o fetch
-//quebramos ele em um repository para ficar acessível e organizado em outras
-//partes do código
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+    if (
+        responseBody !== null &&
+        typeof responseBody === "object" &&
+        "todos" in responseBody &&
+        Array.isArray(responseBody.todos)
+    ) {
+        return {
+            todos: responseBody.todos.map((todo: unknown) => {
+                if (todo === null && typeof todo !== "object") {
+                    throw new Error("Invalid todo from API");
+                }
+
+                const { id, content, done, date } = todo as {
+                    id: string;
+                    content: string;
+                    date: string;
+                    done: string;
+                };
+
+                return {
+                    id,
+                    content,
+                    date: new Date(date),
+                    done: String(done).toLowerCase() === "true", //coloca-se como String para evitar bug, pois quando o false vem como string, ele retorna true
+                };
+            }),
+        };
+    }
+
+    return {
+        todos: [],
+    };
+}
